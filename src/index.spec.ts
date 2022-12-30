d115138616506373f470b62855251aaaa4db7cd6
Emmanuel Mahuni <emahuni@gmail.com>
2022-12-30 13:57:36 +0200

    feat: use vitest, fix tests & bump pinia to latest ver

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,259 +1,296 @@
-import expect from 'expect'
-import { persistentKey, PiniaHistory } from './index'
-import { createPinia, defineStore, setActivePinia } from 'pinia'
-import { ref } from 'vue'
-import 'mock-local-storage'
+import { persistentKey, PiniaHistory } from './index';
+import { defineStore, setActivePinia } from 'pinia';
+import { createTestingPinia } from '@pinia/testing';
+import { ref } from 'vue';
+import 'mock-local-storage';
+
+import { vitest, describe, beforeEach, it, expect } from 'vitest';
 
 describe('Pinia History', () => {
   const useOptionsStore = defineStore('one', {
-    state: () => ({ count: 1 }),
+    state:   () => ({ count: 1 }),
     history: true,
-  })
-
+  });
+  
   const useFullOptionsStore = defineStore({
-    id: 'one',
-    state: () => ({ count: 1 }),
+    id:      'one',
+    state:   () => ({ count: 1 }),
     history: true,
-  })
-
+  });
+  
   const useSetupStore = defineStore(
-    'one',
-    () => {
-      const count = ref(1)
-      return { count }
-    },
-    { history: true }
-  )
-
+      'one',
+      () => {
+        const count = ref(1);
+        return { count };
+      },
+      { history: true },
+  );
+  
   beforeEach(() => {
-    const pinia = createPinia()
-    pinia._p.push(PiniaHistory)
-    setActivePinia(pinia)
-    localStorage.clear()
-  })
-
+    const pinia = createTestingPinia({
+      createSpy: vitest.fn,
+      plugins:   [PiniaHistory],
+    });
+    setActivePinia(pinia);
+    localStorage.clear();
+  });
+  
   it('should add undo method', async () => {
     // Check if typings work for all types of stores.
-
-    const fullOptionsStore = useFullOptionsStore()
-    const optionsStore = useOptionsStore()
-    const setupStore = useSetupStore()
-    expect(fullOptionsStore.undo).toBeDefined()
-    expect(optionsStore.undo).toBeDefined()
-    expect(setupStore.undo).toBeDefined()
-  })
-
+    
+    const fullOptionsStore = useFullOptionsStore();
+    const optionsStore = useOptionsStore();
+    const setupStore = useSetupStore();
+    expect(fullOptionsStore.undo).toBeDefined();
+    expect(optionsStore.undo).toBeDefined();
+    expect(setupStore.undo).toBeDefined();
+  });
+  
   it('should add redo method', async () => {
     // Check if typings work for all types of stores.
-
-    const fullOptionsStore = useFullOptionsStore()
-    const optionsStore = useOptionsStore()
-    const setupStore = useSetupStore()
-    expect(fullOptionsStore.redo).toBeDefined()
-    expect(optionsStore.redo).toBeDefined()
-    expect(setupStore.redo).toBeDefined()
-  })
-
+    
+    const fullOptionsStore = useFullOptionsStore();
+    const optionsStore = useOptionsStore();
+    const setupStore = useSetupStore();
+    expect(fullOptionsStore.redo).toBeDefined();
+    expect(optionsStore.redo).toBeDefined();
+    expect(setupStore.redo).toBeDefined();
+  });
+  
   it('should add canUndo getter', async () => {
     // Check if typings work for all types of stores.
-
-    const fullOptionsStore = useFullOptionsStore()
-    const optionsStore = useOptionsStore()
-    const setupStore = useSetupStore()
-    expect(fullOptionsStore.canUndo).toBeDefined()
-    expect(optionsStore.canUndo).toBeDefined()
-    expect(setupStore.canUndo).toBeDefined()
-  })
-
+    
+    const fullOptionsStore = useFullOptionsStore();
+    const optionsStore = useOptionsStore();
+    const setupStore = useSetupStore();
+    expect(fullOptionsStore.canUndo).toBeDefined();
+    expect(optionsStore.canUndo).toBeDefined();
+    expect(setupStore.canUndo).toBeDefined();
+  });
+  
   it('should add canRedo getter', async () => {
     // Check if typings work for all types of stores.
-
-    const fullOptionsStore = useFullOptionsStore()
-    const optionsStore = useOptionsStore()
-    const setupStore = useSetupStore()
-    expect(fullOptionsStore.canRedo).toBeDefined()
-    expect(optionsStore.canRedo).toBeDefined()
-    expect(setupStore.canRedo).toBeDefined()
-  })
-
-  it('should undo', async () => {
-    const setupStore = useSetupStore()
-    setupStore.$patch({ count: 2 })
-    setupStore.count = 5
-    setupStore.$patch({ count: 3 })
-    expect(setupStore.count).toEqual(3)
-    setupStore.undo()
-    expect(setupStore.count).toEqual(5)
-    setupStore.undo()
-    expect(setupStore.count).toEqual(2)
-    setupStore.undo()
-    expect(setupStore.count).toEqual(1)
-    expect(setupStore.canUndo).toBeFalsy()
-  })
-
+    
+    const fullOptionsStore = useFullOptionsStore();
+    const optionsStore = useOptionsStore();
+    const setupStore = useSetupStore();
+    expect(fullOptionsStore.canRedo).toBeDefined();
+    expect(optionsStore.canRedo).toBeDefined();
+    expect(setupStore.canRedo).toBeDefined();
+  });
+  
+  it('should undo using direct mutations', async () => {
+    const setupStore = useSetupStore();
+    setupStore.count = 2;
+    setupStore.count = 5;
+    setupStore.count = 3;
+    expect(setupStore.count).toEqual(3);
+    setupStore.undo();
+    expect(setupStore.count).toEqual(5);
+    setupStore.undo();
+    expect(setupStore.count).toEqual(2);
+    setupStore.undo();
+    expect(setupStore.count).toEqual(1);
+    expect(setupStore.canUndo).toBeFalsy();
+  });
+  
+  it('should undo using patch mutations', async () => {
+    const setupStore = useSetupStore();
+    setupStore.$patch({ count: 2 });
+    setupStore.$patch({ count: 5 });
+    setupStore.$patch({ count: 3 });
+    expect(setupStore.count).toEqual(3);
+    setupStore.undo();
+    expect(setupStore.count).toEqual(5);
+    setupStore.undo();
+    expect(setupStore.count).toEqual(2);
+    setupStore.undo();
+    expect(setupStore.count).toEqual(1);
+    expect(setupStore.canUndo).toBeFalsy();
+  });
+  
   it('should work with objects (key removal)', async () => {
     const useStore = defineStore('complex', {
-      state: () => ({ someObject: { someKey: 1, someOtherKey: 4 } }),
+      state:   () => ({ someObject: { someKey: 1, someOtherKey: 4 } }),
       history: true,
-    })
-
-    const store = useStore()
-
+    });
+    
+    const store = useStore();
+    
     // @ts-ignore
-    delete store.someObject.someOtherKey
-
-    expect(store.$state).toMatchObject({ someObject: { someKey: 1 } })
-
-    store.undo()
-
+    delete store.someObject.someOtherKey;
+    
+    expect(store.$state).toMatchObject({ someObject: { someKey: 1 } });
+    
+    store.undo();
+    
     expect(store.$state).toMatchObject({
       someObject: { someKey: 1, someOtherKey: 4 },
-    })
-
-    store.redo()
-
-    expect(store.$state).toMatchObject({ someObject: { someKey: 1 } })
-  })
-
-  it('should redo', async () => {
-    const setupStore = useSetupStore()
-    setupStore.$patch({ count: 2 })
-    setupStore.count = 5
-    setupStore.$patch({ count: 3 })
-    setupStore.undo()
-    setupStore.undo()
-    setupStore.undo()
-    expect(setupStore.count).toEqual(1)
-    setupStore.redo()
-    expect(setupStore.count).toEqual(2)
-    setupStore.redo()
-    expect(setupStore.count).toEqual(5)
-    setupStore.redo()
-    expect(setupStore.count).toEqual(3)
-    expect(setupStore.canRedo).toBeFalsy()
-  })
-
+    });
+    
+    store.redo();
+    
+    expect(store.$state).toMatchObject({ someObject: { someKey: 1 } });
+  });
+  
+  it('should redo using direct mutations', async () => {
+    const setupStore = useSetupStore();
+    setupStore.count = 2;
+    setupStore.count = 5;
+    setupStore.count = 3;
+    setupStore.undo();
+    setupStore.undo();
+    setupStore.undo();
+    expect(setupStore.count).toEqual(1);
+    setupStore.redo();
+    expect(setupStore.count).toEqual(2);
+    setupStore.redo();
+    expect(setupStore.count).toEqual(5);
+    setupStore.redo();
+    expect(setupStore.count).toEqual(3);
+    expect(setupStore.canRedo).toBeFalsy();
+  });
+  
+  it('should redo using patch mutations', async () => {
+    const setupStore = useSetupStore();
+    setupStore.$patch({ count: 2 });
+    setupStore.$patch({ count: 5 });
+    setupStore.$patch({ count: 3 });
+    setupStore.undo();
+    setupStore.undo();
+    setupStore.undo();
+    expect(setupStore.count).toEqual(1);
+    setupStore.redo();
+    expect(setupStore.count).toEqual(2);
+    setupStore.redo();
+    expect(setupStore.count).toEqual(5);
+    setupStore.redo();
+    expect(setupStore.count).toEqual(3);
+    expect(setupStore.canRedo).toBeFalsy();
+  });
+  
   it('should invalidate redo', async () => {
-    const setupStore = useSetupStore()
-    setupStore.$patch({ count: 2 })
-    setupStore.count = 5
-    setupStore.undo()
-    expect(setupStore.canRedo).toBeTruthy()
-    setupStore.count = 4
-    expect(setupStore.canRedo).toBeFalsy()
-    expect(setupStore.canUndo).toBeTruthy()
-    setupStore.undo()
-    expect(setupStore.count).toEqual(2)
-    expect(setupStore.canRedo).toBeTruthy()
-    setupStore.redo()
-    expect(setupStore.count).toEqual(4)
-  })
-
+    const setupStore = useSetupStore();
+    setupStore.$patch({ count: 2 });
+    setupStore.$patch({ count: 5 });
+    setupStore.undo();
+    expect(setupStore.canRedo).toBeTruthy();
+    setupStore.$patch({ count: 4 });
+    expect(setupStore.canRedo).toBeFalsy();
+    expect(setupStore.canUndo).toBeTruthy();
+    setupStore.undo();
+    expect(setupStore.count).toEqual(2);
+    expect(setupStore.canRedo).toBeTruthy();
+    setupStore.redo();
+    expect(setupStore.count).toEqual(4);
+  });
+  
   it('should only store up to `option.max` items', async () => {
-    const max = 5
-
+    const max = 5;
+    
     const store = defineStore(
-      'one',
-      () => {
-        const count = ref(1)
-        return { count }
-      },
-      { history: { max } }
-    )()
-
+        'one',
+        () => {
+          const count = ref(1);
+          return { count };
+        },
+        { history: { max } },
+    )();
+    
     for (let i = 0; i < max + 1; i++) {
-      store.$patch({ count: i })
+      store.$patch({ count: i });
     }
-
+    
     for (let i = 0; i < max; i++) {
-      store.undo()
+      store.undo();
     }
-
-    expect(store.count).toEqual(0)
-    expect(store.canUndo).toBeFalsy()
-
+    
+    expect(store.count).toEqual(0);
+    expect(store.canUndo).toBeFalsy();
+    
     for (let i = 0; i < max; i++) {
-      store.redo()
+      store.redo();
     }
-
-    expect(store.count).toEqual(5)
-    expect(store.canRedo).toBeFalsy()
-  })
-
+    
+    expect(store.count).toEqual(5);
+    expect(store.canRedo).toBeFalsy();
+  });
+  
   it('should persist the history', async () => {
     const store = defineStore(
-      'one',
-      () => {
-        const count = ref(1)
-        return { count }
-      },
-      { history: { persistent: true } }
-    )()
-
-    const undoKey = persistentKey(store, 'undo')
-    const redoKey = persistentKey(store, 'redo')
-
-    store.$patch({ count: 2 })
-
-    expect(localStorage.getItem(undoKey)).toEqual('eyJjb3VudCI6MX0=')
-    expect(localStorage.getItem(redoKey)).toEqual('')
-
-    store.undo()
-
-    expect(store.count).toEqual(1)
-    expect(localStorage.getItem(undoKey)).toEqual('')
-    expect(localStorage.getItem(redoKey)).toEqual('eyJjb3VudCI6Mn0=')
-
-    store.redo()
-
-    expect(store.count).toEqual(2)
-    expect(localStorage.getItem(undoKey)).toEqual('eyJjb3VudCI6MX0=')
-    expect(localStorage.getItem(redoKey)).toEqual('')
-  })
-
+        'one',
+        () => {
+          const count = ref(1);
+          return { count };
+        },
+        { history: { persistent: true } },
+    )();
+    
+    const undoKey = persistentKey(store, 'undo');
+    const redoKey = persistentKey(store, 'redo');
+    
+    store.$patch({ count: 2 });
+    
+    expect(localStorage.getItem(undoKey)).toEqual('eyJjb3VudCI6MX0=');
+    expect(localStorage.getItem(redoKey)).toEqual('');
+    
+    store.undo();
+    
+    expect(store.count).toEqual(1);
+    expect(localStorage.getItem(undoKey)).toEqual('');
+    expect(localStorage.getItem(redoKey)).toEqual('eyJjb3VudCI6Mn0=');
+    
+    store.redo();
+    
+    expect(store.count).toEqual(2);
+    expect(localStorage.getItem(undoKey)).toEqual('eyJjb3VudCI6MX0=');
+    expect(localStorage.getItem(redoKey)).toEqual('');
+  });
+  
   it('should persist the history with custom strategy', async () => {
-    const storage: any = {}
-
+    const storage: any = {};
+    
     const store = defineStore(
-      'one',
-      () => {
-        const count = ref(1)
-        return { count }
-      },
-      {
-        history: {
-          persistent: true,
-          persistentStrategy: {
-            get(store, type) {
-              return storage[store.$id]?.[type].split(',')
-            },
-            set(store, type, value) {
-              storage[store.$id] ??= {}
-              storage[store.$id][type] = value.join(',')
-            },
-            remove(store, type) {
-              delete storage[store.$id]?.[type]
+        'one',
+        () => {
+          const count = ref(1);
+          return { count };
+        },
+        {
+          history: {
+            persistent:         true,
+            persistentStrategy: {
+              get (store, type) {
+                return storage[store.$id]?.[type].split(',');
+              },
+              set (store, type, value) {
+                storage[store.$id] = storage[store.$id] ?? {};
+                storage[store.$id][type] = value.join(',');
+              },
+              remove (store, type) {
+                delete storage[store.$id]?.[type];
+              },
             },
           },
         },
-      }
-    )()
-
-    store.$patch({ count: 2 })
-
-    expect(storage[store.$id].undo).toEqual('{"count":1}')
-    expect(storage[store.$id].redo).toEqual('')
-
-    store.undo()
-
-    expect(store.count).toEqual(1)
-    expect(storage[store.$id].undo).toEqual('')
-    expect(storage[store.$id].redo).toEqual('{"count":2}')
-
-    store.redo()
-
-    expect(store.count).toEqual(2)
-    expect(storage[store.$id].undo).toEqual('{"count":1}')
-    expect(storage[store.$id].redo).toEqual('')
-  })
-})
+    )();
+    
+    store.$patch({ count: 2 });
+    
+    expect(storage[store.$id].undo).toEqual('{"count":1}');
+    expect(storage[store.$id].redo).toEqual('');
+    
+    store.undo();
+    
+    expect(store.count).toEqual(1);
+    expect(storage[store.$id].undo).toEqual('');
+    expect(storage[store.$id].redo).toEqual('{"count":2}');
+    
+    store.redo();
+    
+    expect(store.count).toEqual(2);
+    expect(storage[store.$id].undo).toEqual('{"count":1}');
+    expect(storage[store.$id].redo).toEqual('');
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,131 +1,133 @@
-import { PiniaPluginContext, Store, SubscriptionCallbackMutation } from 'pinia'
-import { computed, ComputedRef, reactive } from 'vue'
-import lzutf8 from 'lzutf8'
-const { compress, decompress } = lzutf8
+import { PiniaPluginContext, Store, SubscriptionCallbackMutation } from 'pinia';
+import { computed, ComputedRef, reactive } from 'vue';
+import lzutf8 from 'lzutf8';
+
+const { compress, decompress } = lzutf8;
 
 declare module 'pinia' {
   export interface DefineStoreOptionsBase<S, Store> {
-    history?: boolean | Partial<HistoryPluginOptions>
+    history?: boolean | Partial<HistoryPluginOptions>;
   }
-
+  
   export function defineStore<
-    Id extends string,
-    S extends StateTree = {},
-    G extends _GettersTree<S> = {},
-    A = {},
-    H = false
-  >(
-    id: Id,
-    options: Omit<DefineStoreOptions<Id, S, G, A>, 'id'> & { history: H }
+      Id extends string,
+      S extends StateTree = {},
+      G extends _GettersTree<S> = {},
+      A = {},
+      H = false
+  > (
+      id: Id,
+      options: Omit<DefineStoreOptions<Id, S, G, A>, 'id'> & { history: H },
   ): H extends false
-    ? StoreDefinition<Id, S, G, A>
-    : StoreDefinition<Id, S, G & HistoryPluginGetters, A & HistoryPluginActions>
-
+      ? StoreDefinition<Id, S, G, A>
+      : StoreDefinition<Id, S, G & HistoryPluginGetters, A & HistoryPluginActions>
+  
   export function defineStore<
-    Id extends string,
-    S extends StateTree = {},
-    G extends _GettersTree<S> = {},
-    A = {},
-    H = false
-  >(
-    options: DefineStoreOptions<Id, S, G, A> & { history: H }
+      Id extends string,
+      S extends StateTree = {},
+      G extends _GettersTree<S> = {},
+      A = {},
+      H = false
+  > (
+      options: DefineStoreOptions<Id, S, G, A> & { history: H },
   ): H extends false
-    ? StoreDefinition<Id, S, G, A>
-    : StoreDefinition<Id, S, G & HistoryPluginGetters, A & HistoryPluginActions>
-
-  export function defineStore<Id extends string, SS, H = false>(
-    id: Id,
-    storeSetup: () => SS,
-    options?: DefineSetupStoreOptions<
-      Id,
-      StoreState<SS>,
-      StoreGetters<SS>,
-      StoreActions<SS>
-    > & { history: H }
+      ? StoreDefinition<Id, S, G, A>
+      : StoreDefinition<Id, S, G & HistoryPluginGetters, A & HistoryPluginActions>
+  
+  export function defineStore<Id extends string, SS, H = false> (
+      id: Id,
+      storeSetup: () => SS,
+      options?: DefineSetupStoreOptions<
+          Id,
+          StoreState<SS>,
+          StoreGetters<SS>,
+          StoreActions<SS>
+      > & { history: H },
   ): H extends false
-    ? StoreDefinition<Id, StoreState<SS>, StoreGetters<SS>, StoreActions<SS>>
-    : StoreDefinition<
-        Id,
-        StoreState<SS>,
-        StoreGetters<SS> & HistoryPluginGetters,
-        StoreActions<SS> & HistoryPluginActions
+      ? StoreDefinition<Id, StoreState<SS>, StoreGetters<SS>, StoreActions<SS>>
+      : StoreDefinition<
+          Id,
+          StoreState<SS>,
+          StoreGetters<SS> & HistoryPluginGetters,
+          StoreActions<SS> & HistoryPluginActions
       >
 }
 
 export interface HistoryPluginOptions {
-  max: number
-  persistent: boolean
+  max: number;
+  persistent: boolean;
   persistentStrategy: {
-    get(store: HistoryStore, type: 'undo' | 'redo'): string[] | undefined
-    set(store: HistoryStore, type: 'undo' | 'redo', value: string[]): void
-    remove(store: HistoryStore, type: 'undo' | 'redo'): void
-  }
+    get (store: HistoryStore, type: 'undo' | 'redo'): string[] | undefined
+    set (store: HistoryStore, type: 'undo' | 'redo', value: string[]): void
+    remove (store: HistoryStore, type: 'undo' | 'redo'): void
+  };
 }
 
 export interface HistoryPluginActions {
-  undo(): void
-  redo(): void
+  undo (): void;
+  
+  redo (): void;
 }
 
 export interface HistoryPluginGetters {
-  canUndo: ComputedRef<boolean>
-  canRedo: ComputedRef<boolean>
+  canUndo: ComputedRef<boolean>;
+  canRedo: ComputedRef<boolean>;
 }
 
 export interface History extends HistoryPluginOptions {
-  done: string[]
-  undone: string[]
-  current: string
-  trigger: boolean
+  done: string[];
+  undone: string[];
+  current: string;
+  trigger: boolean;
 }
 
 export interface HistoryStore
-  extends Store,
-    HistoryPluginGetters,
-    HistoryPluginActions {}
+    extends Store,
+        HistoryPluginGetters,
+        HistoryPluginActions {}
 
 /**
  * Base options for the history.
  */
 export const BasePiniaHistoryOptions = {
-  max: 10,
-  persistent: false,
+  max:                10,
+  persistent:         false,
   persistentStrategy: {
-    get(store: HistoryStore, type: 'undo' | 'redo'): string[] | undefined {
+    get (store: HistoryStore, type: 'undo' | 'redo'): string[] | undefined {
       if (typeof localStorage !== undefined) {
-        const key = persistentKey(store, type)
-        const value = localStorage.getItem(key)
-        if (!value) return
-
+        const key = persistentKey(store, type);
+        const value = localStorage.getItem(key);
+        if (!value) return;
+        
         const string = decompress(value, {
           inputEncoding: 'Base64',
-        }) as string
-
-        return string.split(',')
+        }) as string;
+        
+        return string.split(',');
       }
     },
-    set(store: HistoryStore, type: 'undo' | 'redo', value: string[]) {
+    set (store: HistoryStore, type: 'undo' | 'redo', value: string[]) {
       if (typeof localStorage !== undefined) {
-        const key = persistentKey(store, type)
-
-        const string = value.join(',')
-
+        const key = persistentKey(store, type);
+        
+        const string = value.join(',');
+        
         localStorage.setItem(
-          key,
-          compress(string, {
-            outputEncoding: 'Base64',
-          })
-        )
+            key,
+            compress(string, {
+              outputEncoding: 'Base64',
+            }),
+        );
       }
     },
-    remove(store: HistoryStore, type: 'undo' | 'redo') {
+    remove (store: HistoryStore, type: 'undo' | 'redo') {
       if (typeof localStorage !== undefined) {
-        const key = persistentKey(store, type)
-        localStorage.removeItem(key)
+        const key = persistentKey(store, type);
+        localStorage.removeItem(key);
       }
     },
   },
-}
+};
 
 /**
  * Merge the user options with the default ones.
@@ -133,11 +135,11 @@ export const BasePiniaHistoryOptions = {
  * @param options
  * @returns {HistoryPluginOptions}
  */
-function mergeOptions(options: boolean | Partial<HistoryPluginOptions>) {
+function mergeOptions (options: boolean | Partial<HistoryPluginOptions>) {
   return {
     ...BasePiniaHistoryOptions,
     ...(typeof options === 'boolean' ? {} : options),
-  } as HistoryPluginOptions
+  } as HistoryPluginOptions;
 }
 
 /**
@@ -146,17 +148,17 @@ function mergeOptions(options: boolean | Partial<HistoryPluginOptions>) {
  * @param store
  * @param $history
  */
-function persistHistory(store: HistoryStore, $history: History) {
+function persistHistory (store: HistoryStore, $history: History) {
   const {
     persistent,
     persistentStrategy: { set },
     done,
     undone,
-  } = $history
-
+  } = $history;
+  
   if (persistent) {
-    set(store, 'undo', done)
-    set(store, 'redo', undone)
+    set(store, 'undo', done);
+    set(store, 'redo', undone);
   }
 }
 
@@ -167,27 +169,27 @@ function persistHistory(store: HistoryStore, $history: History) {
  * @param $history
  * @returns
  */
-function createPersistentHistory($store: HistoryStore, $history: History) {
+function createPersistentHistory ($store: HistoryStore, $history: History) {
   const {
     persistent,
     persistentStrategy: { get, set, remove },
-  } = $history
-
+  } = $history;
+  
   if (persistent) {
     if ($history.done.length === 0) {
-      $history.done = get($store, 'undo') ?? []
+      $history.done = get($store, 'undo') ?? [];
     } else {
-      set($store, 'undo', $history.done)
+      set($store, 'undo', $history.done);
     }
-
+    
     if ($history.undone.length === 0) {
-      $history.undone = get($store, 'redo') ?? []
+      $history.undone = get($store, 'redo') ?? [];
     } else {
-      set($store, 'redo', $history.undone)
+      set($store, 'redo', $history.undone);
     }
   } else {
-    remove($store, 'undo')
-    remove($store, 'redo')
+    remove($store, 'undo');
+    remove($store, 'redo');
   }
 }
 
@@ -199,35 +201,35 @@ function createPersistentHistory($store: HistoryStore, $history: History) {
  * @param method
  * @returns
  */
-function createStackMethod(
-  $store: HistoryStore,
-  $history: History,
-  method: 'undo' | 'redo'
+function createStackMethod (
+    $store: HistoryStore,
+    $history: History,
+    method: 'undo' | 'redo',
 ) {
-  const can = method === 'undo' ? 'canUndo' : 'canRedo'
+  const can = method === 'undo' ? 'canUndo' : 'canRedo';
   return () => {
     if ($store[can]) {
-      const { undone, done, max, current } = $history
-      const stack = method === 'undo' ? done : undone
-      const reverseStack = method === 'undo' ? undone : done
-
-      const state = stack.pop()
-
-      if (state === undefined) return
-
+      const { undone, done, max, current } = $history;
+      const stack = method === 'undo' ? done : undone;
+      const reverseStack = method === 'undo' ? undone : done;
+      
+      const state = stack.pop();
+      
+      if (state === undefined) return;
+      
       if (reverseStack.length >= max) {
-        reverseStack.splice(0, 1)
+        reverseStack.splice(0, 1);
       }
-
-      reverseStack.push(current)
-
-      $history.trigger = false
-      $store.$patch(JSON.parse(state))
-      $history.trigger = true
-
-      persistHistory($store, $history)
+      
+      reverseStack.push(current);
+      
+      $history.trigger = false;
+      $store.$patch(JSON.parse(state));
+      $history.trigger = true;
+      
+      persistHistory($store, $history);
     }
-  }
+  };
 }
 
 /**
@@ -238,25 +240,22 @@ function createStackMethod(
  * @param $history
  * @returns
  */
-function createWatcher($store: HistoryStore, $history: History) {
-  return (
-    _mutation: SubscriptionCallbackMutation<any>,
-    state: HistoryStore['$state']
-  ) => {
-    const { trigger, max, done, current } = $history
-
+function createWatcher ($store: HistoryStore, $history: History) {
+  return (_mutation: SubscriptionCallbackMutation<any>, state: HistoryStore['$state']) => {
+    const { trigger, max, done, current } = $history;
+    
     if (trigger) {
       if (done.length >= max) {
-        done.splice(0, 1)
+        done.splice(0, 1);
       }
-
-      done.push(current)
-      $history.undone = []
-      persistHistory($store, $history)
+      
+      done.push(current);
+      $history.undone = [];
+      persistHistory($store, $history);
     }
-
-    $history.current = JSON.stringify(state)
-  }
+    
+    $history.current = JSON.stringify(state);
+  };
 }
 
 /**
@@ -266,8 +265,8 @@ function createWatcher($store: HistoryStore, $history: History) {
  * @param method
  * @returns
  */
-export function persistentKey(store: Store, method: 'undo' | 'redo') {
-  return `pinia-plugin-history-${store.$id}-${method}` as const
+export function persistentKey (store: Store, method: 'undo' | 'redo') {
+  return `pinia-plugin-history-${store.$id}-${method}` as const;
 }
 
 /**
@@ -284,34 +283,34 @@ export function persistentKey(store: Store, method: 'undo' | 'redo') {
  * ```
  */
 export const PiniaHistory = ({ options, store }: PiniaPluginContext) => {
-  const { history } = options
-
+  const { history } = options;
+  
   if (history) {
-    const { max, persistent, persistentStrategy } = mergeOptions(history)
-
-    const $store = store as HistoryStore
-
+    const { max, persistent, persistentStrategy } = mergeOptions(history);
+    
+    const $store = store as HistoryStore;
+    
     const $history = reactive({
       max,
       persistent,
       persistentStrategy,
-      done: [],
-      undone: [],
-      current: JSON.stringify($store.$state),
-      trigger: true,
+      done:        [],
+      undone:      [],
+      current:     JSON.stringify($store.$state),
+      trigger:     true,
       resetUndone: false,
-    } as History)
-
-    store.canUndo = computed(() => $history.done.length > 0)
-
-    store.canRedo = computed(() => $history.undone.length > 0)
-
-    store.undo = createStackMethod($store, $history, 'undo')
-
-    store.redo = createStackMethod($store, $history, 'redo')
-
-    store.$subscribe(createWatcher($store, $history))
-
-    createPersistentHistory($store, $history)
+    } as History);
+    
+    store.canUndo = computed(() => $history.done.length > 0);
+    
+    store.canRedo = computed(() => $history.undone.length > 0);
+    
+    store.undo = createStackMethod($store, $history, 'undo');
+    
+    store.redo = createStackMethod($store, $history, 'redo');
+    
+    store.$subscribe(createWatcher($store, $history));
+    
+    createPersistentHistory($store, $history);
   }
-}
+};
